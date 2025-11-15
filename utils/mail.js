a3dcc7fd5f4d24876bc6e92a8f9961f25ace29bd
Roi-git-code <wagundaisack@gmail.com.com>
2025-11-16 02:47:32 +0300

    Update Nodemailer transporter: added tls.rejectUnauthorized=false for Render

--- utils/mail.js
+++ utils/mail.js
@@ -12,7 +12,12 @@ const transporter = nodemailer.createTransport({
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
+  },
+
+tls: {
+    rejectUnauthorized: false
   }
+
 });
 
 // Test transporter connection (optional)
